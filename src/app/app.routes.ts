fb471da43e231ba4fa04b00886a7576c7a336464
Rasmus Bergkvist <bergqvist.rasmus@gmail.com>
2026-06-23 10:43:24 +0200

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Courses } from './pages/courses/courses';
+import { Schedule } from './pages/schedule/schedule';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"", component: Home},
+    {path: "courses", component: Courses},
+    {path: "kurser", component: Courses},
+    {path: "schedule", component: Schedule},
+    {path: "schema", component: Schedule},
+    {path: "ramschema", component: Schedule},
+    {path:"**", redirectTo: "", pathMatch:"full"}
+];

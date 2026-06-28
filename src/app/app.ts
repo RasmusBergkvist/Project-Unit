d30304d3aeccd5308cc4ed7c599c995c4c1cb08d
Rasmus Bergkvist <bergqvist.rasmus@gmail.com>
2026-06-28 21:34:17 +0200

    Skapat partial Main Footer samt uppdaterat responsiviteten i HomeComponent.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MainNav } from "./partials/main-nav/main-nav";
+import { MainFooter } from "./partials/main-footer/main-footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MainNav],
+  imports: [RouterOutlet, MainNav, MainFooter],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/app/partials/main-footer/main-footer.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
+
+@Component({
+  selector: 'app-main-footer',
+  imports: [RouterLink],
+  templateUrl: './main-footer.html',
+  styleUrl: './main-footer.scss',
+})
+export class MainFooter {}

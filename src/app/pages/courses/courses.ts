6ebd4ab023c162088d1e829fdb483b0380cd3367
Rasmus Bergkvist <bergqvist.rasmus@gmail.com>
2026-07-07 13:19:22 +0200

    CourseComponent: Filterar på kursnamn och kurskod.

--- src/app/pages/courses/courses.ts
+++ src/app/pages/courses/courses.ts
@@ -1,10 +1,11 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { Course } from '../../models/course.interface';
 import { CourseService } from '../../services/course.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-courses',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './courses.html',
   styleUrl: './courses.scss',
 })
@@ -13,10 +14,24 @@ export class CoursesComponent {
   //Signals
   courses = signal<Course[]>([]);
   error = signal<string | null>(null);
-
+  filterText = signal<string>("");
 
   //Läser in service
   courseService = inject(CourseService);
+
+//Filtrera kurser
+filterCourses = computed(()=> {
+  const filter = this.filterText().trim().toLowerCase();
+  if(!filter) {
+    return this.courses();
+  }
+
+  return this.courses().filter(c =>
+    c.courseCode.toLowerCase().includes(filter) ||
+    c.courseName.toLowerCase().includes(filter)
+  )
+});
+
 
   //Anropar loadCouses
   ngOnInit() {
@@ -28,7 +43,6 @@ export class CoursesComponent {
     try {
       const response = await this.courseService.getCourses();
       this.courses.set(response)
-      console.table(response)
     } catch(error) {
       console.error(error) 
       this.error.set("Kurserna kunde inte laddas. Försök igen senare")

1113036d16e7ff3c5737a998f1ab9298c61e3a56
Rasmus Bergkvist <bergqvist.rasmus@gmail.com>
2026-07-07 21:20:20 +0200

    CourseComponent: Filtrerar efter ämnen.

--- src/app/pages/courses/courses.ts
+++ src/app/pages/courses/courses.ts
@@ -2,6 +2,7 @@ import { Component, computed, inject, signal } from '@angular/core';
 import { Course } from '../../models/course.interface';
 import { CourseService } from '../../services/course.service';
 import { FormsModule } from '@angular/forms';
+import { NgClass } from "../../../../node_modules/@angular/common/types/_common_module-chunk";
 
 @Component({
   selector: 'app-courses',
@@ -15,21 +16,45 @@ export class CoursesComponent {
   courses = signal<Course[]>([]);
   error = signal<string | null>(null);
   filterText = signal<string>("");
+  selectedSubject = signal<string>("");
 
   //Läser in service
   courseService = inject(CourseService);
 
-//Filtrera kurser
-filterCourses = computed(()=> {
-  const filter = this.filterText().trim().toLowerCase();
-  if(!filter) {
-    return this.courses();
-  }
+//Lista med alla unika ämnen till select
+allSubjects = computed(() =>{
+  const subjects = [... new Set(this.courses().map(course => course.subject))].sort();
 
-  return this.courses().filter(c =>
-    c.courseCode.toLowerCase().includes(filter) ||
-    c.courseName.toLowerCase().includes(filter)
-  )
+  return subjects;
+
+})
+
+//Filtrerar kurser baserat på söktext och valt ämne
+filterCourses = computed(()=> {
+
+  //Hämtar söktext
+  const filter = this.filterText().trim().toLowerCase();
+  
+  //Hämtar valt ämne
+  const subject = this.selectedSubject();
+  let courses = this.courses();
+
+    //Filtrering på kurskod och kursnamn
+    if (filter) {
+      courses = courses.filter(c =>
+        c.courseCode.toLowerCase().includes(filter) ||
+        c.courseName.toLowerCase().includes(filter)
+      );
+    }
+
+    //Filtrering på ämne
+    if(subject) {
+      courses = courses.filter(c =>
+        c.subject === subject
+      );
+    }
+
+    return courses;
 });
 
 

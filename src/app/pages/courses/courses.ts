26cdd74808716d4adbc27b16807db81432b829f9
Rasmus Bergkvist <bergqvist.rasmus@gmail.com>
2026-07-11 23:49:30 +0200

    CoursesComponent: Filtrerar nivåer samt sorterar på kurskod, kursnamn, poäng och ämne. Även styling av felmeddelande.

--- src/app/pages/courses/courses.ts
+++ src/app/pages/courses/courses.ts
@@ -2,7 +2,6 @@ import { Component, computed, inject, signal } from '@angular/core';
 import { Course } from '../../models/course.interface';
 import { CourseService } from '../../services/course.service';
 import { FormsModule } from '@angular/forms';
-import { NgClass } from "../../../../node_modules/@angular/common/types/_common_module-chunk";
 
 @Component({
   selector: 'app-courses',
@@ -15,29 +14,40 @@ export class CoursesComponent {
   //Signals
   courses = signal<Course[]>([]);
   error = signal<string | null>(null);
+
+  //Signals filtrering
   filterText = signal<string>("");
   selectedSubject = signal<string>("");
+  selectedLevels = signal<string[]>([]);
+
+  //Signals sortering
+  sortBy = signal<"courseCode" | "courseName" | "points" | "subject">("courseCode");
+  sortDirection = signal<"asc" | "desc">("asc");
+
 
   //Läser in service
   courseService = inject(CourseService);
 
-//Lista med alla unika ämnen till select
-allSubjects = computed(() =>{
-  const subjects = [... new Set(this.courses().map(course => course.subject))].sort();
 
-  return subjects;
 
-})
+  //Filtrerar kurser baserat på söktext, valt ämne och nivå
+  filterAndSortCourses = computed(() => {
 
-//Filtrerar kurser baserat på söktext och valt ämne
-filterCourses = computed(()=> {
+    //Hämtar söktext
+    const filter = this.filterText().trim().toLowerCase();
 
-  //Hämtar söktext
-  const filter = this.filterText().trim().toLowerCase();
-  
-  //Hämtar valt ämne
-  const subject = this.selectedSubject();
-  let courses = this.courses();
+    //Hämtar valt ämne
+    const subject = this.selectedSubject();
+
+    //Hämtar valda nivåer
+    const levels = this.selectedLevels();
+
+
+    let courses = this.courses();
+
+    //Hämtar vilket fält som ska sorteras och i vilken riktning
+    const sortBy = this.sortBy();
+    const sortDirection = this.sortDirection();
 
     //Filtrering på kurskod och kursnamn
     if (filter) {
@@ -48,30 +58,103 @@ filterCourses = computed(()=> {
     }
 
     //Filtrering på ämne
-    if(subject) {
+    if (subject) {
       courses = courses.filter(c =>
         c.subject === subject
       );
     }
 
-    return courses;
-});
+    //Filtrering på nivå
+    if (levels.length > 0) {
+      courses = courses.filter(c =>
+        levels.includes(c.level)
+      );
+    }
+
+    //Skapar kopia av kurserna till sorteringen
+    const sortedCourses = [...courses];
+
+    //Sorterar kurserna efter valt fält
+    sortedCourses.sort((a, b) => {
+
+      //Sortering efter tal
+      if (sortBy === "points") {
+        return sortDirection === "asc"
+          ? a.points - b.points : b.points - a.points
+      }
 
 
-  //Anropar loadCouses
-  ngOnInit() {
-    this.loadCourses();
+      //Sortering efter strängar
+      const sortA = a[sortBy];
+      const sortB = b[sortBy];
+
+      //Jämför kurserna och sorterar i fallande eller stigande ordning.
+      return sortDirection === "asc"
+        ? sortA.localeCompare(sortB) : sortB.localeCompare(sortA);
+
+    });
+
+    //Returnerar de sorterade kurserna
+    return sortedCourses;
+  });
+
+  //Lista med alla unika ämnen till select
+  allSubjects = computed(() => {
+    const subjects = [... new Set(this.courses().map(course => course.subject))].sort();
+
+    return subjects;
+
+  });
+
+
+  //Ändrar sorteringsfält och sorteringsordning
+  changeSortOrder(sortField: "courseCode" | "courseName" | "points" | "subject") {
+    if (this.sortBy() === sortField) {
+      //Uppdaterar sorteringen och växlar mellan stigande och fallande ordning
+      this.sortDirection.update(sortOrder => sortOrder === "asc" ? "desc" : "asc");
+    } else {
+      //Byter sorteringsfält
+      this.sortBy.set(sortField);
+      //Återställer sortering till stigande ordning
+      this.sortDirection.set("asc")
+    }
+
   }
 
-  //Laddar kuserna
+  //Ändrar valda nivåer
+  changeLevels(level: string) {
+    //Hämtar valda nivåer
+    const levels = this.selectedLevels();
+
+    if (levels.includes(level)) {
+      //Tar bort vald nivå
+      this.selectedLevels.set(
+        levels.filter(l => l !== level)
+      );
+    } else {
+      //Lägger till vald nivå
+      this.selectedLevels.set([...levels, level]);
+    }
+  }
+
+
+  //Anropar loadCourses
+  ngOnInit() {
+    this.loadCourses();
+
+  }
+
+  //Laddar kurserna
   async loadCourses() {
     try {
       const response = await this.courseService.getCourses();
-      this.courses.set(response)
-    } catch(error) {
-      console.error(error) 
+      this.courses.set(response);
+    } catch (error) {
+      console.error(error)
       this.error.set("Kurserna kunde inte laddas. Försök igen senare")
     }
 
   }
+
+
 }
